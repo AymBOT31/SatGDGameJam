7e7b3e7ae815efb8ccb8591f3e787b105560ce41
AymBOT31 <89139960+AymBOT31@users.noreply.github.com>
2022-06-18 11:39:38 +0600

    dewq

--- Player.gd
+++ Player.gd
@@ -1,16 +1,31 @@
 extends KinematicBody2D
 
+export (int) var speed = 450
+export (int) var jump_speed = 100
+export (int) var gravity = 100
+var velocity = Vector2.ZERO
+onready var animsprite = $AnimatedSprite
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func get_input():
+	velocity.x = 0
+	if Input.is_action_pressed("ui_right"):
+			animsprite.flip_h = false
+			animsprite.play("Run")
+			velocity.x += speed
+	elif Input.is_action_pressed("ui_left"):
+			animsprite.flip_h = true
+			animsprite.play("Run")
+			velocity.x -= speed
+	else:
+		animsprite.play("Idle")
 
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
+			velocity.y = -jump_speed
+	else:
+		gravity = 2000
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
