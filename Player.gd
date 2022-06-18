55c797cfa4d72660867d53b5c04e0e173229760a
AymBOT31 <89139960+AymBOT31@users.noreply.github.com>
2022-06-18 12:49:24 +0600

    3e

--- Player.gd
+++ Player.gd
@@ -8,25 +8,22 @@ onready var animsprite = $AnimatedSprite
 
 func get_input():
 	velocity.x = 0
-	if Input.is_action_pressed("ui_right"):
-			animsprite.flip_h = false
+	if velocity.y != 0:
+		animsprite.play("Jump")
+		if Input.is_action_pressed("ui_right"):
+				animsprite.set_flip_h(false)
+				velocity.x += speed
+		elif Input.is_action_pressed("ui_left"):
+				animsprite.set_flip_h(true)
+				velocity.x -= speed
+	else:
+		if Input.is_action_pressed("ui_right"):
+			animsprite.set_flip_h(false)
 			animsprite.play("Run")
 			velocity.x += speed
-	elif Input.is_action_pressed("ui_left"):
-			animsprite.flip_h = true
+		elif Input.is_action_pressed("ui_left"):
+			animsprite.set_flip_h(true)
 			animsprite.play("Run")
 			velocity.x -= speed
-	else:
-		animsprite.play("Idle")
-
-func _physics_process(delta):
-	get_input()
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
-	if Input.is_action_just_pressed("ui_up"):
-		if is_on_floor():
-			animsprite.play("Jump")
-			velocity.y = -jump_speed
-	else:
-		gravity = 250
-
+		else:
+			animsprite.play("Idle")
